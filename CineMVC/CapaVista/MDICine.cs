dcf5a019b380f284af628d11ca27bef60eecd1d7
Heydi <sucelyalvarez@hotmail.com>
2022-04-20 12:19:04 -0600

    MDI de Cine, Autenticación de Usuario

---
+++ CineMVC/CapaVista/MDICine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+using CapaVistaSeguridadHSC;
+
+namespace CapaVista
+{
+	public partial class MDICine : Form
+	{
+		CapaControlador.Controlador controlador = new CapaControlador.Controlador();
+		public MDICine()
+		{
+			InitializeComponent();
+		}
+
+		private void MDICine_Load(object sender, EventArgs e)
+		{
+			frmLoginHSC form = new frmLoginHSC();
+			if (form.ShowDialog() == DialogResult.OK)
+			{
+				txtUsuario.Text = form.usuario();
+			}
+			else
+			{
+				this.Close();
+			}
+		}
+
+		private void cerrarSesiónToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			frmLoginHSC form = new frmLoginHSC();
+			if (form.ShowDialog() == DialogResult.OK)
+			{
+				txtUsuario.Text = form.usuario();
+			}
+			else
+			{ this.Close(); }
+		}
+
+		private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
